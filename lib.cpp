c3fdb2ee5218574c29f81c9baa684e2266ce63ee
FFraaa <91596644+FFraaa@users.noreply.github.com>
2021-12-11 11:49:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,11 +1,11 @@
 #include "lib.h"
 #include <cstring>
 bool ricerca (char lista[10][20], char nome[1][20], int &pos){
-  for(int i=0; i<10; i++)
-  {
+  for(int i=0; i<10; i++){
     if(strcmp(lista[i] nome[0])==0){
-      pos=1;
+      pos=i;
       return true;
-    }}
+    }
+  }
   return false;
 }
